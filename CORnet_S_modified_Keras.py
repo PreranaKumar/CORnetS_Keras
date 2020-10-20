43dc0feb6b1c96173980bc504cb4f6f2076bc1d2
mstettler <michael.stettler@cin.uni-tuebingen.de>
2020-10-20 09:53:10 +0200

    finished CORNet architecture

--- CORnet_S_modified_Keras.py
+++ CORnet_S_modified_Keras.py
@@ -28,7 +28,7 @@ from matplotlib.pyplot import imshow
 
 #Create 'toy' dataset to check whether model compiles
 
-X = np.random.randint(0, 1, size=(50,224,224,3))
+X = np.random.randint(0, 1, size=(50, 224, 224, 3))
 Y = np.random.choice([0, 1], size=(50,))
 #print(X.shape)
 #print(Y.shape)
@@ -57,51 +57,48 @@ class CORblock_S:
     scale = 4  # Scale of the bottleneck convolution channels
     chanDim = -1 #If format is 'channels last'
     
-    def __init__(self, out_channels, inputs, times=1):
+    def __init__(self, out_channels, layer_name, times=1):
         #super().__init__()
+        self.layer_name = layer_name
         
         
         self.out_channels = out_channels   
         self.inputs = inputs
         self.times = times
         
-        self.conv_input = tf.keras.layers.Conv2D(self.out_channels,(1,1)) 
-        self.skip = tf.keras.layers.Conv2D(self.out_channels,(1,1), strides = 2, use_bias=False)
-        self.conv1 = tf.keras.layers.Conv2D(self.out_channels*self.scale,(1,1),use_bias=False)
-        #self.act1 = tf.keras.layers.Activation("relu")
-        self.conv2 = tf.keras.layers.Conv2D(self.out_channels*self.scale, (3,3), strides = 2,use_bias=False)
-        #self.act2 = tf.keras.layers.Activation("relu")
-        self.conv3 = tf.keras.layers.Conv2D(self.out_channels,(1,1),use_bias=False)
-        #self.act3 = tf.keras.layers.Activation("relu")
+        self.conv_input = tf.keras.layers.Conv2D(self.out_channels, (1,1), padding='SAME', name=layer_name+'_convInp')
+        self.skip = tf.keras.layers.Conv2D(self.out_channels, (1,1), strides=2, padding='SAME', use_bias=False, name=layer_name+'_skip')
+        self.conv1 = tf.keras.layers.Conv2D(self.out_channels*self.scale, (1,1), padding='SAME', use_bias=False, name=layer_name+'_conv1')
+        self.conv2 = tf.keras.layers.Conv2D(self.out_channels*self.scale, (3,3), padding='SAME', strides=1, use_bias=False, name=layer_name+'_conv2')
+        self.conv3 = tf.keras.layers.Conv2D(self.out_channels,(1,1), padding='SAME', use_bias=False, name=layer_name+'_conv3')
              
     #@staticmethod 
-    def CORblock_forward(self):
+    def CORblock_forward(self, input):
         
-        x = self.conv_input(self.inputs) 
+        x = self.conv_input(input)
 
         for t in range(self.times):
             
             if t == 0:
-                self.skip = tf.keras.layers.BatchNormalization(axis=-1)(self.skip(x))
-                conv2_stride = 2
+                skip = tf.keras.layers.BatchNormalization(axis=-1, name=self.layer_name+'_BNSkip')(self.skip(x))
             else:
-                self.skip = x
-                conv2_stride = 1
-                
+                skip = x
+
             x = self.conv1(x)
-            x = tf.keras.layers.BatchNormalization(axis=-1)(x)
-            x = tf.keras.layers.Activation("relu")(x)
+            x = tf.keras.layers.BatchNormalization(axis=-1, name=self.layer_name+'_'+str(t)+'_BN1')(x)
+            x = tf.keras.layers.Activation("relu", name=self.layer_name+'_'+str(t)+'_A1')(x)
 
             x = self.conv2(x)
-            x = tf.keras.layers.BatchNormalization(axis=-1)(x)
-            x = tf.keras.layers.Activation("relu")(x)
+            if t == 0:  # adding a stride=2 for t=0
+                x = tf.keras.layers.MaxPool2D(pool_size=(1, 1), strides=2, padding='SAME')(x)
+            x = tf.keras.layers.BatchNormalization(axis=-1, name=self.layer_name+'_'+str(t)+'_BN2')(x)
+            x = tf.keras.layers.Activation("relu", name=self.layer_name+'_'+str(t)+'_A2')(x)
 
             x = self.conv3(x)
-            x = tf.keras.layers.BatchNormalization(axis=-1)(x)
+            x = tf.keras.layers.BatchNormalization(axis=-1, name=self.layer_name+'_'+str(t)+'_BN3')(x)
 
-            x += self.skip #Check tf.concat() also
-            x = tf.keras.layers.Activation("relu")(x)
-            #output = x
+            x += skip
+            x = tf.keras.layers.Activation("relu", name=self.layer_name+'_'+str(t)+'_AOutput')(x)
 
         return x
 
@@ -111,45 +108,35 @@ class CORblock_S:
 
 #Define & implement model 
 
-#Functional API format
-inputs = tf.keras.Input(shape = (224,224,3))
-x = tf.keras.layers.Conv2D(64, (7,7), strides=2, padding='valid',use_bias=False)(inputs)#Check
+# Functional API format
+inputs = tf.keras.Input(shape=(224,224,3))
+
+# V1
+x = tf.keras.layers.Conv2D(64, (7,7), strides=2, padding='SAME', use_bias=False)(inputs)#Check
 x = tf.keras.layers.BatchNormalization(axis=-1)(x)
 x = tf.keras.layers.Activation("relu")(x)
-x = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid')(x)
-x = tf.keras.layers.Conv2D(64, (3,3), strides=1, padding='same',use_bias=False)(x)
+x = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=2, padding='SAME')(x)
+x = tf.keras.layers.Conv2D(64, (3,3), strides=1, padding='SAME', use_bias=False)(x)
 x = tf.keras.layers.BatchNormalization(axis=-1)(x)
 x = tf.keras.layers.Activation("relu")(x)
-    
-#V2 
-inputs = x
-x = CORblock_S(128, inputs, times =2).CORblock_forward()
 
-#V4
-inputs = x
-x = CORblock_S(256, inputs, times =4).CORblock_forward()
-
-#IT
-inputs = x
-x = CORblock_S(512, inputs, times =2).CORblock_forward()
+# weight sharing for V2-V4-IT
+x = CORblock_S(128, layer_name="V2", times=2).CORblock_forward(x)
+x = CORblock_S(256, layer_name="V4", times=4).CORblock_forward(x)
+x = CORblock_S(512, layer_name="IT", times=2).CORblock_forward(x)
     
 x = GlobalAveragePooling2D()(x)
 x = tf.keras.layers.Flatten()(x)
 x = tf.keras.layers.Dense(1000)(x)
     
-model = tf.keras.Model(inputs,x,name = 'CORnetS')
+model = tf.keras.Model(inputs, x, name='CORnetS')
 
 opt = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.9) #Add correct learning rate
-model.compile(loss='categorical_crossentropy', optimizer= opt, metrics=['accuracy'])
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer=opt, metrics=['accuracy'])
+
+print(model.summary())
     
 model.fit(trainX, trainY, validation_data=(testX, testY), epochs=2, batch_size=10) #Paper- 43 epochs training
 score = model.evaluate(testX, testY, verbose=0)
 
 model.summary()
-
-
-# In[ ]:
-
-
-
-
